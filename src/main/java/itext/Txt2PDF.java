7ceaf83ffc591680de514dbb211cc0c5faa974bf
Peter_shw <787297031@qq.com>
2020-06-09 14:56:57 +0800

    itext text --> pdf

---
+++ src/main/java/itext/Txt2PDF.java
@@ -0,0 +1,109 @@
+package itext;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.*;
+
+public class Txt2PDF {
+    public static void text2pdf(String text, String pdf) throws DocumentException, IOException {
+        Document document = new Document();
+        OutputStream os = new FileOutputStream(new File(pdf));
+        PdfWriter.getInstance(document, os);
+        document.open();
+
+
+        BaseFont baseFont = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+        Font font = new Font(baseFont);
+
+        String encoding = getFilecharset(new File(text));
+
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(new File(text)), encoding);
+        BufferedReader bufferedReader = new BufferedReader(isr);
+        String str = "";
+        while ((str = bufferedReader.readLine()) != null) {
+            document.add(new Paragraph(str, font));
+        }
+        document.close();
+    }
+
+    //判断编码格式方法
+    private static  String getFilecharset(File sourceFile) {
+        String charset = "GBK";
+        byte[] first3Bytes = new byte[3];
+        try {
+            boolean checked = false;
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(sourceFile));
+            bis.mark(0);
+            int read = bis.read(first3Bytes, 0, 3);
+            if (read == -1) {
+                return charset; //文件编码为 ANSI
+            } else if (first3Bytes[0] == (byte) 0xFF
+                    && first3Bytes[1] == (byte) 0xFE) {
+                charset = "UTF-16LE"; //文件编码为 Unicode
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xFE
+                    && first3Bytes[1] == (byte) 0xFF) {
+                charset = "UTF-16BE"; //文件编码为 Unicode big endian
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xEF
+                    && first3Bytes[1] == (byte) 0xBB
+                    && first3Bytes[2] == (byte) 0xBF) {
+                charset = "UTF-8"; //文件编码为 UTF-8
+                checked = true;
+            }
+            bis.reset();
+            if (!checked) {
+                int loc = 0;
+                while ((read = bis.read()) != -1) {
+                    loc++;
+                    if (read >= 0xF0)
+                        break;
+                    if (0x80 <= read && read <= 0xBF) // 单独出现BF以下的，也算是GBK
+                        break;
+                    if (0xC0 <= read && read <= 0xDF) {
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF) // 双字节 (0xC0 - 0xDF)
+                            // (0x80
+                            // - 0xBF),也可能在GB编码内
+                            continue;
+                        else
+                            break;
+                    } else if (0xE0 <= read && read <= 0xEF) {// 也有可能出错，但是几率较小
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF) {
+                            read = bis.read();
+                            if (0x80 <= read && read <= 0xBF) {
+                                charset = "UTF-8";
+                                break;
+                            } else
+                                break;
+                        } else
+                            break;
+                    }
+                }
+            }
+            bis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return charset;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String PDFTIMEDIR = "D:/";
+        String text = "F:\\_project\\_test_project\\util_class_collection\\src\\main\\resources\\txt\\news.txt";
+        String pdf = PDFTIMEDIR + "news.pdf";
+        System.out.println(text + " encoding:" +getFilecharset(new File(text)));
+
+        text2pdf(text, pdf);
+        String text2 = "F:\\_project\\_test_project\\util_class_collection\\src\\main\\resources\\txt\\xiaoshuo.txt";
+        String pdf2 = PDFTIMEDIR + "xiaoshuo.pdf";
+        text2pdf(text2, pdf2);
+        System.out.println(text2 + " encoding:" +getFilecharset(new File(text2)));
+    }
+}
