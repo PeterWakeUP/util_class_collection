e69a539e991dc4ab35fc6f1accaf6f39dbab742e
Peter_shw <787297031@qq.com>
2020-07-06 17:05:19 +0800

    父节点排序

--- src/main/java/tree/TreeUtil.java
+++ src/main/java/tree/TreeUtil.java
@@ -17,8 +17,9 @@ public class TreeUtil {
         for (Map.Entry<Long, TreeNode> en : map.entrySet()) {
             TreeNode node = en.getValue();
             TreeNode pTreeNode = map.get(node.getPid());
-            //含有父节点就是子节点
+
             if (Objects.nonNull(pTreeNode)) {
+                //含有父节点就是子节点
                 List<TreeNode> sons = pTreeNode.getChildren();
                 if (Objects.isNull(sons)) {
                     sons = new ArrayList<>();
@@ -34,8 +35,16 @@ public class TreeUtil {
                 }
                 sons.add(i, node);
 
-            } else {//不是子节点就是父节点
-                nodes.add(node);
+            } else {
+                //不是子节点就是父节点
+                int i;
+                for(i=0; i<nodes.size(); i++){
+                    //按顺序插入
+                    if(nodes.get(i).getId() > node.getId()){
+                        break;
+                    }
+                }
+                nodes.add(i, node);
             }
         }
         return nodes;
@@ -53,28 +62,28 @@ public class TreeUtil {
         map.put(node13.getId(), node13);
         TreeNode node12 = new TreeNode(12,"hengli", 1);
         map.put(node12.getId(), node12);
-        TreeNode node1 = new TreeNode(1,"东莞", 0);
-        map.put(node1.getId(), node1);
         TreeNode node2 = new TreeNode(2,"东坑", 1);
         map.put(node2.getId(), node2);
-        TreeNode node3 = new TreeNode(3,"黄屋村", 2);
-        map.put(node3.getId(), node3);
         TreeNode node4 = new TreeNode(4,"茶山", 1);
         map.put(node4.getId(), node4);
+        TreeNode node9 = new TreeNode(9,"龙华", 7);
+        map.put(node9.getId(), node9);
         TreeNode node5 = new TreeNode(5,"东城", 1);
         map.put(node5.getId(), node5);
+        TreeNode node8 = new TreeNode(8,"宝安", 7);
+        map.put(node8.getId(), node8);
         TreeNode node6 = new TreeNode(6,"南城", 1);
         map.put(node6.getId(), node6);
         TreeNode node7 = new TreeNode(7,"深圳", 0);
         map.put(node7.getId(), node7);
-        TreeNode node8 = new TreeNode(8,"宝安", 7);
-        map.put(node8.getId(), node8);
-        TreeNode node9 = new TreeNode(9,"龙华", 7);
-        map.put(node9.getId(), node9);
         TreeNode node10 = new TreeNode(10,"福田", 7);
         map.put(node10.getId(), node10);
         TreeNode node11 = new TreeNode(11,"莞城", 1);
         map.put(node11.getId(), node11);
+        TreeNode node3 = new TreeNode(3,"黄屋村", 2);
+        map.put(node3.getId(), node3);
+        TreeNode node1 = new TreeNode(1,"东莞", 0);
+        map.put(node1.getId(), node1);
 
         System.out.println(JSON.toJSONString(TreeUtil.getTree(map)));
 
